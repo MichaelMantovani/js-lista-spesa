37d13b377fdc9c8e3542cc065bf4480e14c2af3c
MichaelMantovani <michmantovani@gmail.com>
2023-06-05 14:40:47 +0200

    Add for cycle

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ const groceryList = document.getElementById('grocery-list');
 
 //Creo un array con la lista della spesa
 const listElement = ['Pane' , 'Carne' , 'Uova' , 'Latte' , 'Formaggio' , 'Cereali'];
+
+
+// FINCHÈ ci sono oggetti nella lista della spesa
+
+let listMessage = ''
+for(let i = 0; i < listElement.length; i++) {
+  listMessage += `<li>${listElement[i]}</li>`
+}
+
+// Stampo nel DOM gli oggetti 
+groceryList.innerHTML = listMessage
