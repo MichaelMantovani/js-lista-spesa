8a21def6d56fcdc8a341e6198eedfb893b4d85d1
MichaelMantovani <michmantovani@gmail.com>
2023-06-05 14:36:17 +0200

    Add page layout and preliminary operations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('JS OK')
+
+
+// Recupero gli elementi dal DOM
+const groceryList = document.getElementById('grocery-list');
+
+
+//Creo un array con la lista della spesa
+const listElement = ['Pane' , 'Carne' , 'Uova' , 'Latte' , 'Formaggio' , 'Cereali'];
